b49441dc57746f14fd31e5fd954455751ede7159
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2024-02-29 23:01:34 -0500

    backend initial commit. Prisma and seed working. Server needs work.

---
+++ backend/server/api/user.js
@@ -0,0 +1,7 @@
+// Routes to access users
+
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
+// const bcrpty = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// require('dotenv').config(); 
