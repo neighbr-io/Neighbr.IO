ae60d7d7c3469a6699097a21ca5a401a5d1ae5c8
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2024-03-02 23:29:14 -0500

    basic routes for user and static, placeholder for project

---
+++ backend/server/api/project.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router(); 
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
+// GET /api/projects - Get all projects
+router.get("/projects", async (req, res) => {
+  try {
+    const projects = await prisma.project.findMany();
+    res.json(projects);
+  } catch (error) {
+    console.error("Failed to get projects:", error);
+    res.status(500).json({ error: "Failed to get projects" });
+  }
+});
+
+// GET /api/projects - Get project by id 
+router.get("/projects/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await prisma.project.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (project) {
+      res.json(project);
+    } else {
+      res.status(404).json({ error: "Project not found" });
+    }
+  } catch (error) {
+    console.error("Failed to get the project:", error);
+    res.status(500).json({ error: "Failed to get the project" });
+  }
+});
+
+module.exports = router;
--- backend/server/server.js
+++ backend/server/server.js
@@ -3,15 +3,17 @@ const express = require('express');
 const projectsRoutes = require('./api/project');
 const usersRoutes = require('./api/user');
 const staticsRoutes = require('./api/static');
+const authRoutes = require('./api/auth');
 
 const app = express();
 const port = 8000; 
 
 app.use(express.json());
 
-app.use('/api', projectsRoutes);
-app.use('/api', usersRoutes);
-app.use('/api', staticsRoutes);
+app.use('/api/projects', projectsRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/static', staticsRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
