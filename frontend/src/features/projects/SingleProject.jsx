98274271a16e37e1325127945757db0d03782cc6
airbornejennie <146116778+airbornejennie@users.noreply.github.com>
2024-03-18 20:28:15 -0500

    working on single page tab details

--- frontend/src/features/projects/SingleProject.jsx
+++ frontend/src/features/projects/SingleProject.jsx
@@ -8,11 +8,11 @@ import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
+// import TabPanel from "@mui/lab/TabPanel";
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
+import LabTabs from "./SingleProjectTab";
 import "./SingleProject.css";
-
 
 const SingleProject = () => {
     const { id } = useParams();
@@ -24,7 +24,7 @@ const SingleProject = () => {
     if (error || !project ) {
         return <div>Error occurred while retrieving data </div>;
     }
-    const { title, subtitle, story, categoryId, faq, updates, funded, expiration, goal } = project;
+    const { title, subtitle, story, category, faq, updates, funded, expiration, goal } = project;
     console.log(expiration);
     const date = new Date(expiration).toDateString();
     console.log(date);
@@ -34,13 +34,16 @@ const SingleProject = () => {
         <h2 className="title">{title}</h2>
         <p className="subtitle">{subtitle}</p>
         <section id="pledge">
-        <p>${funded} pledged of ${goal} goal</p></section>
+        <p>${funded} pledged of ${goal} goal</p>
+        </section>
         <section className="date"><p>Expires: {date}</p></section>
-        
+        <p>
+        <div><LabTabs /></div>
+        </p>
         <p className="story">Story: {story}</p>
         <p className="faq">FAQ: {faq}</p>
         <p className="updates">Updates: {updates}</p>
-        <p className="category">Category: {categoryId}</p>
+        <p className="category">Category: {category}</p>
         <Button id="pledge-button" variant="contained" onClick={() => {
             alert("payment page coming soon!");
         }}>Back This Project</Button>
---
+++ frontend/src/features/projects/SingleProjectTab.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { useGetProjectQuery } from "./projectSlice";
+import { useParams } from "react-router-dom";
+
+
+export default function LabTabs() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const { id } = useParams();
+    const { data: project, error, isLoading } = useGetProjectQuery(id);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    
+    if (error || !project ) {
+        return <div>Error occurred while retrieving data </div>;
+    }
+    const { title, subtitle, story, categoryId, faq, updates, funded, expiration, goal } = project;
+
+  return (
+    <Box sx={{ display:'flex', flexGrow:1, typography: 'body1', width:'800px' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList 
+          orientation="vertical"
+          variant="scrollable" 
+          onChange={handleChange} 
+          aria-label="lab API tabs example"
+          sx={{ borderRight: 1, borderColor: 'divider' }}
+          >
+            <Tab label="Story" value="1" />
+            <Tab label="FAQ" value="2" />
+            <Tab label="Updates" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">{story}</TabPanel>
+        <TabPanel value="2">{faq}</TabPanel>
+        <TabPanel value="3">{updates}</TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
