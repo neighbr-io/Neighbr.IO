9997c73b653273951ebc61325680ff824c7499e3
airbornejennie <146116778+airbornejennie@users.noreply.github.com>
2024-03-27 18:57:03 -0500

    starting dashboard view

--- frontend/src/features/Dashboard/Dashboard.jsx
+++ frontend/src/features/Dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useMeQuery } from './authSlice';
 
 const Dashboard = () => {
+  const { data: user } = useMeQuery();
+  console.log(user);
 
   return (
     <div>
       <h1>Transactions</h1>
+      <h3>Hi {user}!</h3>
       {/* <TransactionsList userId={userId} projectId={projectId} /> */}
     </div>
   );
