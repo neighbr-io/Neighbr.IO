3e120bc5a6f37ae033417e1967acdb222289897b
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2024-04-14 02:04:50 -0400

    na

--- backend/server/server.js
+++ backend/server/server.js
@@ -19,7 +19,6 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "dist")));
-app.use('/api', apiRouter);
 
 app.use('/api/static', staticsRoutes);
 app.use('/api/waitlist', waitlistRoute);
