7038f69abf43bdb956de9ec749c8a734e63e973c
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2024-04-14 01:50:59 -0400

    vite api url

--- frontend/src/app/api.js
+++ frontend/src/app/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const TOKEN = "token";
-const apiUrl = "/api";
+const apiUrl = import.meta.env.VITE_API_URL || '/api'
 console.log(import.meta.env);
 console.log(apiUrl);
 
