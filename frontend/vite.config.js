747563fac93f003de30403fda66a217e0969e125
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2024-04-14 00:31:51 -0400

    corrected build script and paths etc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": "http://localhost:8000",
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../backend/server/dist', // Adjust to match dist folder directory
+  },
   server: {
     proxy: {
       "/api": "http://localhost:8000",
